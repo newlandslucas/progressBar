bc2e61aefa24b95c0a84007bad78eb568d0ef597
newlandslucas <lucasnewlands.m@gmail.com>
2022-07-04 12:49:40 -0300

    Initial Commit

---
+++ progressBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  progressBar
+//
+//  Created by Lucas Machado (Estudos) on 04/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ progressBar/progressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  progressBarApp.swift
+//  progressBar
+//
+//  Created by Lucas Machado (Estudos) on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct progressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
